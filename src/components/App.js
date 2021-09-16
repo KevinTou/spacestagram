3210012979a9b089c74c015190dd6a17757b8242
Kevin Tou <toukevin@gmail.com>
2021-09-16 16:47:03 -0700

    Added basic styles to app container

--- src/components/App.js
+++ src/components/App.js
@@ -4,12 +4,13 @@ import HomeFeed from './HomeFeed.js';
 
 function App() {
   return (
-    <>
+    <div className='app-container'>
       <h1>Spacestagram</h1>
+      <h4>Brought to you by NASA's Astronomy Photo of the Day (APOD) API</h4>
       <Switch>
         <Route exact path='/' component={HomeFeed} />
       </Switch>
-    </>
+    </div>
   );
 }
 
