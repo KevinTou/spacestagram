0f2e4799e5d03cc5873f2bb000db290874ea1c33
Kevin Tou <toukevin@gmail.com>
2021-09-17 09:38:52 -0700

    Added ability to retrieve data

--- src/components/HomeFeed.js
+++ src/components/HomeFeed.js
@@ -1,10 +1,46 @@
-import React from 'react';
+import { useState, useLayoutEffect } from 'react';
+import { fetchImagesWithDemoKey } from '../utils/utility_helpers';
+
+import Loading from './Loading';
 
 const HomeFeed = () => {
+  const [images, setImages] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState('');
+
+  useLayoutEffect(() => {
+    setIsLoading(true);
+    fetchImagesWithDemoKey()
+      .then(({ data }) => {
+        if (Array.isArray(data)) {
+          setImages(data);
+        } else {
+          setImages([data]);
+        }
+        setIsLoading(false);
+      })
+      .catch((error) => {
+        const error_message = error?.response?.data?.error?.message;
+        setError(error_message);
+        setIsLoading(false);
+      });
+  }, []);
+
+  if (isLoading) {
+    return <Loading />;
+  }
+
+  if (error) {
+    alert(error);
+    console.log('firing');
+    setError('');
+  }
+
   return (
-    <div>
+    <main>
       <p>Home Feed</p>
-    </div>
+      {images.length > 0 && console.log(images)}
+    </main>
   );
 };
 
---
+++ src/utils/utility_helpers.js
@@ -0,0 +1,21 @@
+import axios from 'axios';
+
+const BASE_URL_WITH_API_KEY = `https://api.nasa.gov/planetary/apod/?api_key=${process.env.REACT_APP_API_KEY}`;
+
+export function fetchImagesWithDemoKey() {
+  return axios.get(`https://api.nasa.gov/planetary/apod/?api_key=DEMO_KEYs`);
+}
+
+export function fetchImage() {
+  return axios.get(BASE_URL_WITH_API_KEY);
+}
+
+export function fetchImagesWithDate(starting_date = '', ending_date = '') {
+  if (starting_date && ending_date) {
+    return axios.get(
+      `${BASE_URL_WITH_API_KEY}&starting_date=${starting_date}&ending_date=${ending_date}`,
+    );
+  }
+
+  return axios.get(BASE_URL_WITH_API_KEY);
+}
