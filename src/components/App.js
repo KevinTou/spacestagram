7948897c44f34093cb43f68e8af402f646f7e2e3
Kevin Tou <toukevin@gmail.com>
2021-09-19 13:28:25 -0700

    Added styles to app title

--- src/components/App.js
+++ src/components/App.js
@@ -5,8 +5,10 @@ import HomeFeed from './HomeFeed.js';
 function App() {
   return (
     <div className='app-container'>
-      <h1>Spacestagram</h1>
-      <h4>Brought to you by NASA's Astronomy Photo of the Day (APOD) API</h4>
+      <h1 className='app-title'>Spacestagram</h1>
+      <p className='app-description'>
+        Brought to you by NASA's Astronomy Photo of the Day (APOD) API
+      </p>
       <Switch>
         <Route exact path='/' component={HomeFeed} />
       </Switch>
